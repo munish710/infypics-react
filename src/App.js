030125026c27275080862b06cfff6013c6697e0d
munish710 <munish710@gmail.com>
2021-07-14 00:18:20 +0530

    added photo component and display logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
+import Loader from "react-loader-spinner";
+
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
@@ -15,7 +17,7 @@ function App() {
       setLoading(true);
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
+      setPhotos(data);
       setLoading(false);
     } catch (err) {
       setLoading(false);
@@ -26,7 +28,32 @@ function App() {
   useEffect(() => {
     fetchImages();
   }, []);
-  return <h2>stock photos starter</h2>;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form">
+          <input type="text" placeholder="search" className="form-input" />
+          <button type="submit" className="submit-btn" onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((photo) => {
+            return <Photo key={photo.id} {...photo} />;
+          })}
+          {loading && (
+            <Loader type="Oval" color="#617e98" height={80} width={80} />
+          )}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
