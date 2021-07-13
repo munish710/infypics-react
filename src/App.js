cbc7e83c2046b6089655cbc6e7b1ef88138eee43
munish710 <munish710@gmail.com>
2021-07-13 23:36:25 +0530

    setup the fetch functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const fetchImages = async () => {
+    let url;
+    url = `${mainUrl}${clientID}`;
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
   return <h2>stock photos starter</h2>;
 }
 
