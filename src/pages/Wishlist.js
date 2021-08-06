701a541cae3f5b71f3a52668423ba31d7c0f8296
munish710 <munish710@gmail.com>
2021-08-06 16:47:57 +0530

    added empty saved images view

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,18 +1,26 @@
 import React from "react";
 import SavedPhoto from "../components/SavedPhoto";
 import { useAppContext } from "../context/context";
+import empty_images from "../assets/empty_images.svg";
 
 const Wishlist = () => {
   const { savedImages } = useAppContext();
   return (
     <main>
-      <section className="photos">
-        <div className="saved-photos-center">
-          {savedImages.map((image) => {
-            return <SavedPhoto {...image} key={image.id} />;
-          })}
+      {savedImages.length > 0 ? (
+        <section className="photos">
+          <div className="saved-photos-center">
+            {savedImages.map((image) => {
+              return <SavedPhoto {...image} key={image.id} />;
+            })}
+          </div>
+        </section>
+      ) : (
+        <div className="section empty-images">
+          <img src={empty_images} alt="empty box" className="empty-img" />
+          <h3>no saved images</h3>
         </div>
-      </section>
+      )}
     </main>
   );
 };
